ea33b42d98ab04d0f1ef34c1e5f602a9401cb721
ribart Dibart <dibart@gmail.com>
2020-08-04 14:33:56 -0400

    Updates

---
+++ precache-manifest.ba4552137efe07ff7944080d4ef24996.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07729de3fea3aa697b5e337f6d648512",
+    "url": "/momo-botan/index.html"
+  },
+  {
+    "revision": "a68b9eddfd8ef5cf03a3",
+    "url": "/momo-botan/static/css/main.e35977b5.chunk.css"
+  },
+  {
+    "revision": "4fca20673023fcb86886",
+    "url": "/momo-botan/static/js/2.61dcc89b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/momo-botan/static/js/2.61dcc89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68b9eddfd8ef5cf03a3",
+    "url": "/momo-botan/static/js/main.68eee1c2.chunk.js"
+  },
+  {
+    "revision": "597f17b5c56f9fac1910",
+    "url": "/momo-botan/static/js/runtime-main.c7dce079.js"
+  },
+  {
+    "revision": "0b5bd64f21f9d7dc1ac601e2f28bb898",
+    "url": "/momo-botan/static/media/flower.0b5bd64f.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/momo-botan/static/media/shopping-bag.30f07071.svg"
+  }
+]);
